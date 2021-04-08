f684ddc5b58ac53dda5dfb18a96661d08aecee13
Dmitry Kartesv <dmitry.kartsev@cnvrg.io>
2021-04-08 12:09:09 +0300

    wip

--- cmd/cnvrgctl/cluster.go
+++ cmd/cnvrgctl/cluster.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"os"
 	"os/exec"
+	"strings"
 )
 
 var clusterUpParams = []param{
@@ -24,12 +27,32 @@ var clusterUpCmd = &cobra.Command{
 	},
 }
 
+func isUserExists(user string) bool {
+
+	file, err := os.Open("/etc/passwd")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	r := bufio.NewScanner(file)
+	for r.Scan() {
+		lines := r.Text()
+		parts := strings.Split(lines, ":")
+		if parts[0] == user {
+			return true
+		}
+	}
+	return false
+}
+
 func createUser() {
-	argUser := []string{"-m", "-d", "/home/cnvrg", "-s", "/bin/bash", "-p", "paMfuNMgwFAX2", "cnvrg"}
-	userCmd := exec.Command("useradd", argUser...)
-	if out, err := userCmd.CombinedOutput(); err != nil {
-		logrus.Error(err, "tere was an error by adding user cnvrg")
-	} else {
-		logrus.Info(out)
+	if !isUserExists("cnvrg") {
+		argUser := []string{"-m", "-d", "/home/cnvrg", "-s", "/bin/bash", "-p", "paMfuNMgwFAX2", "cnvrg"}
+		userCmd := exec.Command("useradd", argUser...)
+		if out, err := userCmd.CombinedOutput(); err != nil {
+			logrus.Error(err, " there was an error by adding user cnvrg")
+		} else {
+			logrus.Info(out)
+		}
 	}
 }
