106b46defdc9659f48eefc532960f90424e2490c
Dmitry Kartesv <dmitry.kartsev@cnvrg.io>
2021-04-13 22:10:27 +0300

    generate template

--- pkg/assets/assets.go
+++ pkg/assets/assets.go
@@ -7,4 +7,6 @@ func informPkger() {
 	pkger.Include("/pkg/assets/k9s")
 	pkger.Include("/pkg/assets/kubectl")
 	pkger.Include("/pkg/assets/cluster.tpl")
+	pkger.Include("/pkg/assets/cnvrg-crds.yaml")
+	pkger.Include("/pkg/assets/cnvrg-operator.yaml")
 }
